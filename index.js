8f3249f342cbf75999235a13b9f73aea3fde8e7b
Abbas Jafari <abbas.jafari@powercoders.org>
2020-11-16 22:39:02 +0100

    trying out some stuff

--- index.js
+++ index.js
@@ -1,71 +1,108 @@
 const musics = document.querySelectorAll(".musicImg");
+let isPlaying = false;
 musics.forEach(element => {
     element.addEventListener("click", ()=>{
+        console.log(isPlaying);
+
+        if(isPlaying != false && isPlaying != element){
+            
+            isPlaying.classList.toggle("shrink");
+            isPlaying.classList.toggle("hovered");
+            
+            isPlaying.childNodes.forEach(nott=>{
+                changeOverlay(nott, isPlaying);
+                playMusic(nott);
+                rotatation(nott, isPlaying);
+            });
+            isPlaying = false ;                    
+        }
+
+        console.log(isPlaying);
+
+        
+        if(!element.classList.contains("isplaying")){
+            isPlaying = false;
+        }
+        
         element.classList.toggle("shrink");
-        element.classList.toggle("hovered")
+        element.classList.toggle("isplaying");
+        element.classList.toggle("hovered");
         element.childNodes.forEach(t => {
+            
+      
+            rotatation(t, element);
+       
+            changeOverlay(t, element);
 
-            // animations rotatation
-            // animations rotatation
-            // animations rotatation
-
-            if (t.nodeName == "IMG") {
-                    
-                if (t.classList.contains("musicplay")) {
-                    let transformation = window.getComputedStyle(t).getPropertyValue('transform').match(/(-?[0-9\.]+)/g);
-                    let sin = transformation[1];
-                    let cos = transformation[0];
-                    sin = Math.round(Math.asin(sin) * (180/Math.PI));
-                    cos = Math.round(Math.acos(cos) * (180/Math.PI));
-                    let angle = sin;
-                    if(cos>sin){
-                        angle = cos;
-                    }
-                    if(sin < 0){
-                        angle = 180 + Math.abs(sin);
-                        if (cos < 90) {
-                            angle = 360 - Math.abs(sin);
-                        }
-                    }
-                    t.style.transform = `rotate(${angle}deg)`;
-                    t.classList.add("musicstop");
-                    t.classList.remove("musicplay");                    
-                }
-                else{
-                    t.classList.remove("musicstop");
-                    t.classList.add("musicplay");                    
-                }   
-            }
-
-            // play button
-            // play button
-            // play button
-
-
-            if(t.className == "playButton"){   
-                t.childNodes.forEach(img =>{
-                    if(img.nodeName == "IMG"){
-                        if(element.classList.contains("shrink")){
-                            console.log("has")
-                            img.src = "images/pause.png";
-                        }
-                        else{
-                            img.src = "images/playButton.png"
-                        }
-                    }
-                })              
-            }
-            if(t.nodeName == "AUDIO"){
-                if(t.classList.contains("playing")){
-                    t.pause();
-
-                }
-                else{
-                    t.classList.add("playing")
-                    t.play()
-                }
-            }
+            playMusic(t);
+              
         });
+        console.log(isPlaying);
+        console.log("================");
     })
 });
+
+
+const playMusic = (t)=>{
+    if(t.nodeName == "AUDIO"){
+        if(t.classList.contains("playing")){
+            t.pause();
+            t.classList.remove("playing");
+            
+        }
+        else{
+            t.classList.add("playing")
+            t.play()
+        }
+    }
+}
+
+const changeOverlay = (t, elem)=>{
+    if(t.className == "playButton"){   
+        t.childNodes.forEach(img =>{
+            if(img.nodeName == "IMG"){
+                // console.log("sfdsdafsfd")
+                // console.log(elem)
+                if(elem.classList.contains("shrink")){
+                    console.log("has")
+                    img.src = "images/pause.png";
+                }
+                else{
+                    img.src = "images/playButton.png"
+                }
+            }
+        })              
+    }
+}
+
+const rotatation = (t, elem)=>{
+    if (t.nodeName == "IMG") {
+                    
+        if (t.classList.contains("musicplay")) {
+            let transformation = window.getComputedStyle(t).getPropertyValue('transform').match(/(-?[0-9\.]+)/g);
+            let sin = transformation[1];
+            let cos = transformation[0];
+            sin = Math.round(Math.asin(sin) * (180/Math.PI));
+            cos = Math.round(Math.acos(cos) * (180/Math.PI));
+            let angle = sin;
+            if(cos>sin){
+                angle = cos;
+            }
+            if(sin < 0){
+                angle = 180 + Math.abs(sin);
+                if (cos < 90) {
+                    angle = 360 - Math.abs(sin);
+                }
+            }
+            t.style.transform = `rotate(${angle}deg)`;
+            t.classList.add("musicstop");
+            t.classList.remove("musicplay");                   
+        }
+        else{
+            t.classList.remove("musicstop");
+            t.classList.add("musicplay");
+            isPlaying = elem;                   
+        }   
+    }
+}
     
